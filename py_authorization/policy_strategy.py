e94be0aba911697234b93b0dc3ca6061ef663763
Getulio Valentin Sánchez <valentin2507@gmail.com>
2022-11-07 16:14:08 -0300

    Do not raise exception on unimplemented strategy

--- py_authorization/policy_strategy.py
+++ py_authorization/policy_strategy.py
@@ -1,7 +1,8 @@
 from typing import Any, Optional, TypeVar
 
-from py_authorization.context import Context
 from sqlalchemy.orm.query import Query
+
+from py_authorization.context import Context
 
 T = TypeVar("T", bound=object)
 
@@ -10,8 +11,8 @@ class PolicyStrategy:
     def __init__(self, args: dict[str, Any]) -> None:
         self.args = args
 
-    def apply_policies_to_entity(self, entity: Optional[T], context: Context) -> Optional[T]:
-        raise NotImplementedError(f"{context.policy.name} policy cannot be applied on entities")
+    def apply_policies_to_entity(self, entity: T, context: Context) -> Optional[T]:
+        pass
 
     def apply_policies_to_query(self, query: Query, context: Context) -> Query:
-        raise NotImplementedError(f"{context.policy.name} policy cannot be applied on queries")
+        pass
