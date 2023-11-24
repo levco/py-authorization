05b359b84b3e3a4c4783480e3e9747180c3d03e3
Getulio Valentin Sánchez <valentin2507@gmail.com>
2023-11-24 16:44:38 -0300

    Add generic type support for policy strategies

--- py_authorization/__init__.py
+++ py_authorization/__init__.py
@@ -1,6 +1,6 @@
 """Easily manage authorization complex logic"""
 
-__version__ = "1.5.0"
+__version__ = "1.5.1"
 
 from .authorization import Authorization, CheckResponse
 from .context import Context
